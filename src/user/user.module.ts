6a3d8905ed268b7a27bd46dd4f71ccb7e3ca94e2
Jonatas Braz <jonatasbraz@gmail.com>
2022-08-02 10:59:36 -0300

    aplicação e database em containers docker

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { User } from './user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
